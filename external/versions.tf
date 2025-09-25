60c9383a9ea01ddec38f59d71ae6ae32f9fac9b4
Aaron Pejakovic <aaron.pejakovic@outlook.com>
2025-09-25 11:42:08 +0000

    Added versions

--- external/versions.tf
+++ external/versions.tf
@@ -1,11 +1,8 @@
 terraform {
   required_version = "~> 1.7"
   backend "s3" {
-    bucket         = "your-terraform-state-bucket-name" # Replace with your S3 bucket name
-    key            = "path/to/your/terraform.tfstate"   # Replace with your desired key/path
-    region         = "us-east-1"                       # Replace with your AWS region
-    dynamodb_table = "your-terraform-state-lock-table" # Replace with your DynamoDB table name
-    encrypt        = true                              # Optional: Enable server-side encryption
+    bucket         = "pjak-tf-state-store" # Replace with your S3 bucket name
+    key            = "homelab.tfstate"   # Replace with your desired key/path                        # Optional: Enable server-side encryption
   }
   required_providers {
     cloudflare = {
@@ -34,3 +31,4 @@ provider "kubernetes" {
   # Use KUBE_CONFIG_PATH environment variables
   # Or in cluster service account
 }
+
